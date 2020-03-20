40c191b764aab0e399b586fb5e6957be9b15b3fc
tsbarrett89 <tsbarrett89@gmail.com>
2020-03-19 20:03:59 -0600

    added SpellList and SavedSpells routes in app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 
 import Header from './components/Header';
+import SpellList from './components/SpellList';
+import SavedSpells from './components/SavedSpells';
 
 function App() {
   return (
     <div>
       <Header />
+
+      <Route exact path='/' component={SpellList} />
+      <Route path='/spell-sheet' component={SavedSpells} />
     </div>
   );
 }
---
+++ src/components/SpellList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SpellList = () => {
+
+    return (
+        <div>
+            <h3>Spell List</h3>
+        </div>
+    )
+}
+
+export default SpellList
