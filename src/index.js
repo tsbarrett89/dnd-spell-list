fb4e8f298473fbf49ac21a1c4d5e0e478ad59c6e
tsbarrett89 <tsbarrett89@gmail.com>
2020-03-19 19:58:25 -0600

    added router to index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'react-thunk';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import reducer from './reducer';
 
@@ -12,6 +13,8 @@ const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>, document.getElementById('root'));
 
