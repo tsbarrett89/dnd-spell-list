7a02741e396cec6db9c384872064dfd24ecd6c31
tsbarrett89 <tsbarrett89@gmail.com>
2020-03-19 21:49:15 -0600

    added class and spell level select forms

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,13 +4,43 @@ import { connect } from 'react-redux';
 const SearchBar = () => {
 
     return (
-        <form>
-            <input
-                name='query'
-                placeholder='search spells'
-            />
-            <button type='submit'>Search</button>
-        </form>
+        <div>
+            <form>
+                <input
+                    name='query'
+                    placeholder='All Spells'
+                />
+                <button type='submit'>Search by name</button>
+            </form>
+            <form>
+                <select>
+                    <option name=''>Class</option>
+                    <option name='bard'>Bard</option>
+                    <option name='cleric'>Cleric</option>
+                    <option name='druid'>Druid</option>
+                    <option name='paladin'>Paladin</option>
+                    <option name='ranger'>Ranger</option>
+                    <option name='sorcerer'>Sorcerer</option>
+                    <option name='warlock'>Warlock</option>
+                    <option name='wizard'>Wizard</option>
+                </select>
+                <select>
+                    <option name=''>Spell Level</option>
+                    <option name='0'>Cantrips</option>
+                    <option name='1'>first level</option>
+                    <option name='2'>second level</option>
+                    <option name='3'>third level</option>
+                    <option name='4'>fourth level</option>
+                    <option name='5'>fifth level</option>
+                    <option name='6'>sixth level</option>
+                    <option name='7'>seventh level</option>
+                    <option name='8'>eighth level</option>
+                    <option name='9'>ninth level</option>
+                </select>
+                <button type='submit'>Set Search Parameters</button>
+            </form>
+        </div>
+        
     )
 };
 
