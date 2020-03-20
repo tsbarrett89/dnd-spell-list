b5eff497a97e601f8aa988139ccfdea7319a0974
tsbarrett89 <tsbarrett89@gmail.com>
2020-03-19 19:28:39 -0600

    created Header component and mounted in App.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+
+    return (
+        <div>
+            <h1>Dungeons and Dragons Spell List Creator</h1>
+        </div>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'react-thunk';
 
 import reducer from './reducer';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
