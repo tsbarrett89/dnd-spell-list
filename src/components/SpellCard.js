bb66e143814ec57b4c766dc1a66ee8d17e7bdcd6
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-01 15:57:59 -0600

    removed everything but spell name from Spellcard.js

--- src/components/SpellCard.js
+++ src/components/SpellCard.js
@@ -7,8 +7,6 @@ const SpellCard = props => {
     return (
         <div onClick={() => props.fetchSpellData(props.spell.index)}>
             <p>{props.spell.name}</p>
-            {props.spell.isFetching && <p>fetching spell data</p>}
-            {props.spell.desc && <SpellCardExpanded spell={props.spell} />}
         </div>
     )
 };
