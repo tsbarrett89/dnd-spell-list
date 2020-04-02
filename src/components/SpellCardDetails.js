7356be534d1182b252a01d82bbb0fb0539265541
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-01 21:17:27 -0600

    displayed spell information

--- src/components/SpellCardDetails.js
+++ src/components/SpellCardDetails.js
@@ -4,12 +4,25 @@ import axios from 'axios';
 
 const SpellCardExpanded = () => {
     const { spellIndex } = useParams();
-    const [spell, setSpell] = useState({})
+    const [spell, setSpell] = useState({
+        name: '',
+        desc: [],
+        range: '',
+        components: [],
+        ritual: false,
+        duration: '',
+        concentration: false,
+        casting_time: '',
+        level: null,
+        school: { name: '', url: ''},
+        classes: []
+    })
 
     useEffect(() => {
         axios
             .get(`https://cors-anywhere.herokuapp.com/http://dnd5eapi.co/api/spells/${spellIndex}`)
             .then(res => {
+                console.log(res.data)
                 setSpell(res.data)
             })
     }, [])
@@ -17,6 +30,17 @@ const SpellCardExpanded = () => {
     return (
         <div>
             <p>{spell.name}</p>
+            <p>{spell.level} level</p>
+            <p>{spell.school.name}</p>
+            <p>Casting Time: {spell.casting_time}</p>
+            <p>Range: {spell.range}</p>
+            <p>Components: {spell.components.map(comp => comp)}</p>
+            <p>Duration: {spell.duration}</p>
+            <div>{spell.desc.map(p => {
+                return <p>{p}</p>
+            })
+            
+            }</div>
         </div>
     )
 }
---
+++ styling/navBarStyle.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const NavStyled = styled.nav `
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    text-decoration: none;
+`
