e921a7b21755a16993419534367710bfe85dfab0
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-02 15:06:40 -0600

    styled Spell list h3

--- src/styling/spellListStyle.js
+++ src/styling/spellListStyle.js
@@ -4,4 +4,9 @@ export const SpellListStyled = styled.div `
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    h3 {
+        font-size: 2rem;
+        padding: 1% 0;
+    }
 `
